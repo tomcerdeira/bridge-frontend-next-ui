9cbb15488d3643520336d57f9bf437b0bb3b52cc
tom <tcerdeira@itba.edu.ar>
2023-08-21 12:11:01 -0300

    feat: added description to card and made the list scrollable

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Button, Card, CardBody, CardFooter, CardHeader, Divider, Image, Input, Radio, RadioGroup } from "@nextui-org/react";
+import { Button, Card, CardBody, CardHeader, Divider, Image, Input, Radio, RadioGroup } from "@nextui-org/react";
 
 export default function CheckoutPage() {
 	const list = [
@@ -8,47 +8,56 @@ export default function CheckoutPage() {
 		  title: "Orange",
 		  img: "/images/fruit-1.jpeg",
 		  price: "$5.50",
+		  description: "Juicy and vibrant citrus fruit with a sweet-tangy flavor."
 		},
 		{
 		  title: "Tangerine",
 		  img: "/images/fruit-2.jpeg",
 		  price: "$3.00",
+		  description: "Small, easy-to-peel citrus fruit known for its refreshing taste."
 		},
 		{
 		  title: "Raspberry",
 		  img: "/images/fruit-3.jpeg",
 		  price: "$10.00",
+		  description: "Delicate and flavorful berry, perfect for snacking or baking."
 		},
 		{
 		  title: "Lemon",
 		  img: "/images/fruit-4.jpeg",
 		  price: "$5.30",
+		  description: "Sour and zesty citrus fruit commonly used in cooking and beverages."
 		},
 		{
 		  title: "Avocado",
 		  img: "/images/fruit-5.jpeg",
 		  price: "$15.70",
+		  description: "Creamy and nutrient-rich fruit often used in salads and spreads."
 		},
 		{
 		  title: "Lemon 2",
 		  img: "/images/fruit-6.jpeg",
 		  price: "$8.00",
+		  description: "Another variety of the sour and aromatic citrus fruit."
 		},
 		{
 		  title: "Banana",
 		  img: "/images/fruit-7.jpeg",
 		  price: "$7.50",
+		  description: "Naturally sweet and convenient snack loved by people of all ages."
 		},
 		{
 		  title: "Watermelon",
 		  img: "/images/fruit-8.jpeg",
 		  price: "$12.20",
+		  description: "Large and hydrating melon with juicy, refreshing flesh."
 		},
 	  ];
+	  
 	
 	return (
-		<div className="flex gap-6">
-			<div>
+		<div className="gap-6 flex justify-center">
+			<div className="w-[70%] flex-shrink-0 overflow-hidden">
 				<Card className="max-w-[400px]">
 					<CardHeader className="flex gap-3 justify-center">
 						<div className="flex flex-col">
@@ -60,23 +69,39 @@ export default function CheckoutPage() {
 
 					<CardBody className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
 						
-					<div>
+					<div className="overflow-y-auto max-h-[500px]">
 						{list.map((item, index) => (
-							<Card shadow="sm" key={index} isPressable onPress={() => console.log("item pressed")}>
-							<CardBody className="overflow-visible p-0">
-								<Image
-								shadow="sm"
-								radius="lg"
-								width="100%"
-								alt={item.title}
-								className="w-full object-cover h-[140px]"
-								src={item.img}
-								/>
-							</CardBody>
-							<CardFooter className="text-small justify-between">
-								<b>{item.title}</b>
-								<p className="text-default-500">{item.price}</p>
-							</CardFooter>
+							
+							<Card
+							key={index}
+							isBlurred
+							className="border-none bg-background/60 dark:bg-default-100/50 max-w-[610px]"
+							shadow="sm"
+						  >
+								<CardBody>
+									<div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center">
+										<div className="relative col-span-6 md:col-span-4">
+										<Image
+											shadow="sm"
+											radius="lg"
+											width="100%"
+											alt={item.title}
+											className="w-full object-cover h-[140px]"
+											src={item.img}
+										/>
+									</div>
+							
+									<div className="flex flex-col col-span-6 md:col-span-8">
+										<div className="flex justify-between items-start">
+											<div className="flex flex-col gap-0">
+												<h1 className="font-semibold text-foreground/90">{item.title}</h1>
+												<p className="text-small text-foreground/80">{item.price}</p>
+												<h3 className="text-small font-medium mt-2">{item.description}</h3>
+												</div>
+											</div>
+										</div>
+									</div>
+								</CardBody>
 							</Card>
 						))}
 					</div>
@@ -93,62 +118,62 @@ export default function CheckoutPage() {
 				</Card>
 			</div>
 
-			<div>
-			<Card className="max-w-[400px]">
+			<div className="w-[50%] flex-grow">
+				<Card className="max-w-[400px]">
 
-<CardHeader className="flex gap-3 justify-center">
-	<div className="flex flex-col">
-		<p className="text-md">Payment Information</p>
-	</div>
-</CardHeader>
+					<CardHeader className="flex gap-3 justify-center">
+						<div className="flex flex-col">
+							<p className="text-md">Payment Information</p>
+						</div>
+					</CardHeader>
 
-<Divider/>
+					<Divider/>
 
-<CardBody className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+					<CardBody className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
 
-	<Input
-		type="number"
-		label="Card Number"
-		placeholder="1234 5678 9101 1121"
-		labelPlacement="outside"
-		endContent={
-			<Image
-			src="/card-type-logos/amex.png" // Replace with the actual image path
-			alt="Card Type"
-			width={40}
-			height={20} // Adjust the height as needed
-			/>
-		}
-	/>
-	<Input
-		type="text"
-		label="Card Holder Name"
-		placeholder="Your Name"
-		labelPlacement="outside"
-	/>
+					<Input
+						type="number"
+						label="Card Number"
+						placeholder="1234 5678 9101 1121"
+						labelPlacement="outside"
+						endContent={
+							<Image
+							src="/card-type-logos/amex.png" // Replace with the actual image path
+							alt="Card Type"
+							width={40}
+							height={20} // Adjust the height as needed
+							/>
+						}
+					/>
+					<Input
+						type="text"
+						label="Card Holder Name"
+						placeholder="Your Name"
+						labelPlacement="outside"
+					/>
 
-	<div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-		<Input
-			type="date"
-			label="Expiration Date"
-			placeholder="01/01/2023"
-			labelPlacement="outside"
-		/>
-		<Input
-			type="number"
-			label="CVV"
-			placeholder="123"
-			labelPlacement="outside"
-		/>
-	</div>
+					<div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+						<Input
+							type="date"
+							label="Expiration Date"
+							placeholder="01/01/2023"
+							labelPlacement="outside"
+						/>
+						<Input
+							type="number"
+							label="CVV"
+							placeholder="123"
+							labelPlacement="outside"
+						/>
+					</div>
 
-	<Button color="success" variant="shadow">
-		Pay
-	</Button> 
+					<Button color="success" variant="shadow">
+						Pay
+					</Button> 
 
-</CardBody>
+					</CardBody>
 
-</Card>
+				</Card>
 
 			</div>
 
