acfd88d3f86322faca1975aeab17644886ab884f
tom <tcerdeira@itba.edu.ar>
2023-09-12 18:52:42 -0300

    fix: checkout mobile styles fixes

--- app/checkout/[paymentReqId]/checkoutForm.tsx
+++ app/checkout/[paymentReqId]/checkoutForm.tsx
@@ -231,8 +231,8 @@ export default function CheckoutForm({ children, payment_information }: Props) {
 	  
 	
 	return (
-		<div className="gap-6 flex justify-center">
-			<div className="w-[70%] flex-shrink-0 overflow-hidden">
+		<div className="gap-6 flex flex-col md:flex-row justify-center">
+			<div className="w-full md:w-[70%] flex-shrink-0 overflow-hidden">
 				<Card className="max-w-[400px]">
 					<CardHeader className="flex gap-3 justify-center">
 						<div className="flex flex-col">
@@ -249,29 +249,30 @@ export default function CheckoutForm({ children, payment_information }: Props) {
 							className="border-none bg-background/60 dark:bg-default-100/50 max-w-[610px] mb-4"
 							shadow="sm"
 						  >
-								<CardBody>
-									<div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center">
-										<div className="relative col-span-6 md:col-span-4">
-										<Badge content={`x${item.quantity}`} size="lg" color="danger" shape="rectangle" disableOutline>
-											<Image
-												shadow="sm"
-												radius="lg"
-												width="100%"
-												alt={item.name}
-												className="w-full object-cover h-[140px]"
-												src={item.imgUrl}
-											/>
-										</Badge>
-									</div>
+							{/* TODO: fix responsivenes when one on top of the other */}
+								<CardBody> 
+									<div className="flex flex-row gap-6 md:gap-4 items-center justify-left">
+										<div className="col-span-6 md:col-span-4">
+											<Badge content={`x${item.quantity}`} size="lg" color="danger" shape="rectangle" disableOutline>
+												<Image
+													shadow="sm"
+													radius="lg"
+													width="100%"
+													alt={item.name}
+													className="w-full object-contain h-[140px] w-[90px] overflow-hidden"
+													src={item.imgUrl}
+												/>
+											</Badge>
+										</div>
 							
-									<div className="flex flex-col col-span-6 md:col-span-8">
-										<div className="flex justify-between items-start">
-											<div className="flex flex-col gap-0">
-												<h1 className="font-semibold text-foreground/90">{item.name}</h1>
-												<p className="text-small text-foreground/80">${item.unitPrice}</p>
-												<h3 className="text-small font-medium mt-2">{item.description}</h3>
+										<div className="col-span-6 md:col-span-8">
+											<div className="flex justify-between items-start">
+												<div className="flex flex-col gap-0">
+													<h1 className="font-semibold text-foreground/90">{item.name}</h1>
+													<p className="text-small text-foreground/80">${item.unitPrice}</p>
+													<h3 className="text-small font-medium mt-2">{item.description}</h3>
+													</div>
 												</div>
-											</div>
 										</div>
 									</div>
 								</CardBody>
@@ -295,7 +296,7 @@ export default function CheckoutForm({ children, payment_information }: Props) {
 				</Card>
 			</div>
 
-			<div className="w-80 shrink-0">
+			<div className="w-full md:w-80 shrink-0">
 				<Card className="max-w-[400px]">
 					<CardHeader className="flex gap-3 justify-center">
 						<div className="flex flex-col">
