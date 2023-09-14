4e6e64260cdabcebd15c2425eaeaebbaec776a70
tom <tcerdeira@itba.edu.ar>
2023-09-14 20:17:33 -0300

    refac: style changes

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -73,8 +73,8 @@ export default function ForgotPasswordForm() {
                 <Card className="max-w-[400px]">
                     <CardHeader className="flex gap-3 justify-center">
                         <div className="flex flex-col">
+                            <p className="text-xl font-bold text-left mb-4">Forgot Password</p>
                             <p className="text-small text-left opacity-50">Enter the email address associated with your account, and we`ll send you a link to reset your password.</p>
-                            <p className="text-xl font-bold text-left mt-4">Forgot Password</p>
                         </div>
                     </CardHeader>
                     <CardBody className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
@@ -92,8 +92,8 @@ export default function ForgotPasswordForm() {
                         />
                         {error && <p className="mt-4 text-right text-red-400 text-xs">{error.message}</p>}
                         <div className="gap-2 flex flex-col md:flex-row justify-center ">
-                            <Button className="mt-4 w-full" onClick={handleSubmit} isLoading={loadingRequest} color="success" variant="shadow">
-                                Send
+                            <Button className="mt-4 w-full" onClick={handleSubmit} isLoading={loadingRequest} color="warning" variant="shadow">
+                                Reset password
                             </Button> 
                         </div>
                     </CardBody>
