cd51d59dfa18b11b07af9df103b12299fdc0cb07
Reversive <matiaspavan@gmail.com>
2023-09-20 11:31:00 -0300

    misc: consistent coloring for nodes

--- app/flow-builder/components/nodes/baseNode.tsx
+++ app/flow-builder/components/nodes/baseNode.tsx
@@ -5,7 +5,7 @@ const Node = ({ data, hasSource, hasTarget }: ExtendedNodeProps) => {
   const Icon = data;
   return (
     <>
-      <div className="bg-neutral-800 rounded-full p-4 shadow-md">
+      <div className="bg-content1 rounded-full p-4 shadow-md">
         <div className="flex">
           <div className=" px-1 flex justify-center items-center ">
             <Icon className="text-3xl" />
--- app/flow-builder/components/nodes/logicalNode.tsx
+++ app/flow-builder/components/nodes/logicalNode.tsx
@@ -1,11 +1,10 @@
 import { memo } from "react";
-import BaseNode from "./baseNode";
 import { NodeProps, Handle, Position } from "reactflow";
 
 const LogicalNode = ({ data }: NodeProps) => {
   return (
     <>
-      <div className="bg-neutral-800 rounded-xl p-4 shadow-md">
+      <div className="bg-content1 rounded-xl p-4 shadow-md">
         <span className="text-lg font-fira">{data.name.toUpperCase()}</span>
       </div>
       <Handle type="target" position={Position.Left} className="!bg-white" />
