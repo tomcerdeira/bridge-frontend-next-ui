e2972abe2d82ecfb709f93594613f62397b54dfe
tom <tcerdeira@itba.edu.ar>
2023-10-09 13:34:28 -0300

    feat: flow executions by shop in /activity

--- src/api/analytics.ts
+++ src/api/analytics.ts
@@ -5,6 +5,7 @@ import { AnalyticsResponse, FlowExecutionResponse } from "./types";
 
 export const GET_ANALYTICS_BY_SHOP = (shop_id: string) => `/app/private/analytics/shops/${shop_id}`
 export const GET_ANALYTICS_BY_FLOW = (flow_id: string) =>`/app/private/analytics/flows/${flow_id}`
+export const GET_FLOW_EXECUTION_STATUS_BY_SHOP = (shop_id: string) => `/payment/private/flows-exec-status/shops/${shop_id}`
 export const GET_FLOW_EXECUTION_STATUS_BY_FLOW = (flow_id: string) => `/payment/private/flows-exec-status/flows/${flow_id}`
 
 export function useGetAnalyticsByShopId(shop_id: string) {
@@ -21,6 +22,14 @@ export function useGetAnalyticsByFlowId(flow_id: string) {
         fetcher
     );
     return { flow_analytics: data, error, isLoading, getAnalyticsByFlowId: mutate };
+}
+
+export function useGetFlowExecutionStatusByShop(shop_id: string) {
+    const { data, error, isLoading, mutate } = useSWR<FlowExecutionResponse[]>(
+        GET_FLOW_EXECUTION_STATUS_BY_SHOP(shop_id),
+        fetcher
+    );
+    return { flow_analytics: data, error, isLoading, getFlowExecutionStatusByShop: mutate };
 }
 
 export function useGetFlowExecutionStatus(flow_id: string) {
