9085caaaadca7613cf2d64038d883bf5a5b28cc8
tom <tcerdeira@itba.edu.ar>
2023-10-16 13:34:20 -0300

    feat: changed some styles

--- app/flows/flows-table.tsx
+++ app/flows/flows-table.tsx
@@ -78,12 +78,12 @@ export const FlowsTable = ({
               <ModalHeader className="flex flex-col gap-1">Snippets de código</ModalHeader>
               <ModalBody>
                 <p>Implementa la integración copiándolo/pegándolo en el código de tu aplicación.</p>
-                <Tabs aria-label="Options" className="flex justify-center mt-4">
+                <Tabs aria-label="Options" color="primary" className="flex justify-center mt-4">
                   <Tab key="curl" title="CURL">
                     <Snippet symbol="" className="w-full">
                       <pre>
                         { 
-                        `curl -X POST '${process.env.NEXT_PUBLIC_BASE_URL}/payment/private/payments/${clickedFlowId}' \n -H 'authorization: Bearer <TU_TOKEN_DE_ACCESO>' \n -H 'Content-Type: application/json' \n -d '{\n  "amount": 100,\n  "currency": "ARS",\n  "redirectURL": "<TU_URL_DE_REDIRECCION>",\n  "products": [\n     {\n       "name": "Socks",\n       "unitPrice": 100,\n       "description": "Red socks",\n       "imgUrl": "https://media.mysockfactory.ch/1354-thickbox_default/maos-red-plain-socks.jpg",\n       "quantity": 1\n     }\n   ]\n }' `
+                        `curl -X POST '${process.env.NEXT_PUBLIC_BASE_URL}/payment/private/payments/${clickedFlowId}' \n -H 'authorization: Bearer <TU_TOKEN_DE_ACCESO>' \n -H 'Content-Type: application/json' \n -d '{\n  "amount": 100,\n  "currency": "ARS",\n  "redirectURL": <TU_URL_DE_REDIRECCION>,\n  "products": [\n     {\n       "name": "Socks",\n       "unitPrice": 100,\n       "description": "Red socks",\n       "imgUrl": "https://media.mysockfactory.ch/1354-thickbox_default/maos-red-plain-socks.jpg",\n       "quantity": 1\n     }\n   ]\n }' `
                         }
                       </pre>
                     </Snippet>
